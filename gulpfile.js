5163f89172055bc97a0f48df6d7db0071d80f2e5
Steven Weaver <sweaver@ucsd.edu>
2015-10-02 15:49:40 -0700

    reorganizing hyphyvision

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var order = require('gulp-order');
+
+gulp.task('scripts', function() {
+    return gulp.src(['src/**/*.js'])
+    .pipe(order([
+      "datamonkey/datamonkey.js",
+      "datamonkey/*.js",
+      "busted/busted.js",
+      "hivtrace/hivtrace.js",
+      "**/*.js"
+    ]))
+    .pipe(concat('hyphy-vision.js'))
+    .pipe(gulp.dest('./'));
+});
