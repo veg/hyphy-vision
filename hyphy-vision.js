5f6b9ab801e9f43a0d23d45fb663d88ae9196041
Steven Weaver <sweaver@ucsd.edu>
2015-12-18 14:20:01 -0800

    hyphy-vision build

--- hyphy-vision.js
+++ hyphy-vision.js
@@ -2372,68 +2372,6 @@ function saveImage(type, container) {
 
 }
 
-var FadeSummary = React.createClass({displayName: "FadeSummary",
-
-  float_format : d3.format(".2f"),
-
-  countBranchesTested: function(branches_tested) {
-    if(branches_tested) {
-      return branches_tested.split(';').length;
-    } else {
-      return 0;
-    }
-  },
-
-  getDefaultProps : function() {
-    return {
-     subs : []
-    };
-
-  },
-
-  componentDidMount: function() {
-
-    this.setProps({
-       alpha_level : 0.05,
-       sequences : this.props.msa.sequences,
-       subs : this.props.fade_results["TREE_LENGTHS"][0],
-       sites : this.props.msa.sites,
-       model : this.props.fade_results["MODEL_INFO"],
-       grid_desc : this.props.fade_results["GRID_DESCRIPTION"],
-       branches_tested : this.props.fade_results["BRANCHES_TESTED"]
-    });
-
-  },
-
-  render: function() {
-
-    var self = this;
-
-    return (
-          React.createElement("dl", {className: "dl-horizontal"}, 
-            React.createElement("dt", null, "Data summary"), 
-            React.createElement("dd", null, this.props.sequences, " sequences with ", this.props.partitions, " partitions."), React.createElement("dd", null, 
-            React.createElement("div", {className: "alert"}, "These sequences have not been screened for recombination. Selection analyses of alignments with recombinants in them using a single tree may generate ", React.createElement("u", null, "misleading"), " results.")), 
-            this.props.msa.partition_info.map(function(partition, index) {
-              return (React.createElement("div", null, React.createElement("dt", null, "Partition ", partition["partition"]), React.createElement("dd", null, " ", self.float_format(self.props.subs[index]), " subs/ aminoacid  site"), React.createElement("dd", null, partition["endcodon"] - partition["startcodon"], " aminoacids")))
-            }), 
-            React.createElement("dt", null, "Settings"), React.createElement("dd", null, this.props.model), React.createElement("dd", null, this.props.grid_desc), 
-            React.createElement("dd", null, "Directional model applied to ", self.countBranchesTested(this.props.branches_tested), " branches")
-          )
-        )
-  }
-
-});
-
-// Will need to make a call to this
-// omega distributions
-function render_fade_summary(json, msa) {
-  React.render(
-    React.createElement(FadeSummary, {fade_results: json, msa: msa}),
-    document.getElementById("summary-div")
-  );
-}
-
 if (!datamonkey) {
     var datamonkey = {};
 }
@@ -2846,6 +2784,68 @@ datamonkey.fade = function(json) {
   set_handlers('test');
   initial_display();
 
+}
+
+var FadeSummary = React.createClass({displayName: "FadeSummary",
+
+  float_format : d3.format(".2f"),
+
+  countBranchesTested: function(branches_tested) {
+    if(branches_tested) {
+      return branches_tested.split(';').length;
+    } else {
+      return 0;
+    }
+  },
+
+  getDefaultProps : function() {
+    return {
+     subs : []
+    };
+
+  },
+
+  componentDidMount: function() {
+
+    this.setProps({
+       alpha_level : 0.05,
+       sequences : this.props.msa.sequences,
+       subs : this.props.fade_results["TREE_LENGTHS"][0],
+       sites : this.props.msa.sites,
+       model : this.props.fade_results["MODEL_INFO"],
+       grid_desc : this.props.fade_results["GRID_DESCRIPTION"],
+       branches_tested : this.props.fade_results["BRANCHES_TESTED"]
+    });
+
+  },
+
+  render: function() {
+
+    var self = this;
+
+    return (
+          React.createElement("dl", {className: "dl-horizontal"}, 
+            React.createElement("dt", null, "Data summary"), 
+            React.createElement("dd", null, this.props.sequences, " sequences with ", this.props.partitions, " partitions."), React.createElement("dd", null, 
+            React.createElement("div", {className: "alert"}, "These sequences have not been screened for recombination. Selection analyses of alignments with recombinants in them using a single tree may generate ", React.createElement("u", null, "misleading"), " results.")), 
+            this.props.msa.partition_info.map(function(partition, index) {
+              return (React.createElement("div", null, React.createElement("dt", null, "Partition ", partition["partition"]), React.createElement("dd", null, " ", self.float_format(self.props.subs[index]), " subs/ aminoacid  site"), React.createElement("dd", null, partition["endcodon"] - partition["startcodon"], " aminoacids")))
+            }), 
+            React.createElement("dt", null, "Settings"), React.createElement("dd", null, this.props.model), React.createElement("dd", null, this.props.grid_desc), 
+            React.createElement("dd", null, "Directional model applied to ", self.countBranchesTested(this.props.branches_tested), " branches")
+          )
+        )
+  }
+
+});
+
+// Will need to make a call to this
+// omega distributions
+function render_fade_summary(json, msa) {
+  React.render(
+    React.createElement(FadeSummary, {fade_results: json, msa: msa}),
+    document.getElementById("summary-div")
+  );
 }
 
 // TODO: Write documentation
