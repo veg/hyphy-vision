56f28617d654cd45f78cfbe317600d7663d3d967
Stephen Shank <sshank314@gmail.com>
2017-06-27 17:12:50 -0400

    Adds slider p-value filtering to MEME.

--- src/jsx/components/meme_table.jsx
+++ src/jsx/components/meme_table.jsx
@@ -5,23 +5,50 @@ import { DatamonkeyTable } from "./shared_summary.jsx";
 
 
 class MEMETable extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+    this.state = {
+      bodyData: null,
+      value: 10,
+      filter: .1
+    }
+  }
+  componentWillReceiveProps(nextProps){
+    var formatter = d3.format('.2f'),
+      new_rows = nextProps.rows.map(row=>row.map(entry=>formatter(entry)));
+    this.setState({
+      bodyData: new_rows
+    });
+  }
+  handleChange(event){
+    this.setState({value: event.target.value});
+  }
+  handleMouseUp(event){
+    this.setState({filter: event.target.value/100});
+  }
   render(){
     if (this.props.header){
       var headerData = this.props.header.map(pair=>{ return {value:pair[0], abbr:pair[1]}; }),
-        formatter = d3.format('.2f'),
-        bodyData = this.props.rows.map(row=>row.map(entry=>formatter(entry)));
+        bodyData = this.state.bodyData.filter(row=>row[6] < this.state.filter);
     }
+    var self = this;
     return (<div className="row">
       <div className="col-md-12" id="table-tab">
         <h4 className="dm-table-header">
           MEME data
           <span className="glyphicon glyphicon-info-sign" style={{"verticalAlign": "middle", "float":"right"}} aria-hidden="true" data-toggle="popover" data-trigger="hover" title="Actions" data-html="true" data-content="<ul><li>Hover over a column header for a description of its content.</li></ul>" data-placement="bottom"></span>
         </h4>
-          <DatamonkeyTable 
-            headerData={headerData}
-            bodyData={bodyData}
-            paginate={20}
-          />
+        <div style={{width: "200px"}}>
+          <span style={{width: "15%", display: "inline-block", verticalAlign: "middle"}}>{self.state.value/100}</span>
+          <input type="range" id="myRange" value={self.state.value} style={{width: "75%", display: "inline-block", verticalAlign: "middle"}} onChange={this.handleChange} onMouseUp={this.handleMouseUp} />
+        </div>
+        <DatamonkeyTable 
+          headerData={headerData}
+          bodyData={bodyData}
+          paginate={20}
+        />
       </div>
     </div>);
   }
