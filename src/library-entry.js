e1516a322319137c5a2cb2e5b086b96d74a26323
Stephen Shank <sshank314@gmail.com>
2017-10-16 16:03:41 -0400

    Adds FUBAR to entry file for library.

--- src/library-entry.js
+++ src/library-entry.js
@@ -15,3 +15,4 @@ export {default as meme} from './jsx/meme.jsx';
 export {default as prime} from './jsx/prime.jsx';
 export {default as relax} from './jsx/relax.jsx';
 export {default as slac} from './jsx/slac.jsx';
+export {default as fubar} from './jsx/fubar.jsx';
