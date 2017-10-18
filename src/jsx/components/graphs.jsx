fbc6f3f0b6bb2e2b7cca25024acab2c3bfc332e0
Stephen Shank <sshank314@gmail.com>
2017-10-18 10:27:35 -0400

    Adds axis labels to SLAC graphs, gives labels more room.

--- src/jsx/components/graphs.jsx
+++ src/jsx/components/graphs.jsx
@@ -363,10 +363,10 @@ class BaseGraph extends React.Component {
 BaseGraph.defaultProps = {
   width: 800,
   height: 400,
-  marginLeft: 35,
+  marginLeft: 45,
   marginRight: 10,
   marginTop: 10,
-  marginBottom: 35,
+  marginBottom: 45,
   marginXaxis: 5,
   marginYaxis: 5,
   graphData: null,
