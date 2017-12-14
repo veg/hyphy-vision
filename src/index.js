8d015a8601eae476ad98ebf2cc23961041214753
Stephen Shank <sshank314@gmail.com>
2017-12-14 15:41:09 -0500

    Initial setup with React router.

--- src/index.js
+++ src/index.js
@@ -1,43 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const d3 = require('d3');
+import { BSREL } from './jsx/absrel.jsx';
+import { BUSTED } from './jsx/busted.jsx';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 
 require("font-awesome/css/font-awesome.css");
 require('./application.less');
 require('./hyphyvision.css');
 require('./fade/FADE.css');
-
 require('bootstrap');
 require('./datamonkey/datamonkey.js');
 
-class Hello extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {data: null};
-  }
-  componentDidMount(){
-    var self = this,
-      // SDS, 12/12/17: This is a quick hack that needs a better solution...
-      // Use this path for web development
-      path = '/methods/absrel/data/ABSREL.json';
-      // Use this path when testing electron
-      // path = '/Users/stephenshank/Documents/hyphy-vision/methods/absrel/data/ABSREL.json';
-    d3.json(path, (error, data) => {
-      self.setState({data: data});
-    });
-  }
-  render() {
-    return (<div>
-      <h1>HyPhy Vision</h1>
-      <p>A webpack-dev-server and React from scratch</p>
-      {this.state.data ? JSON.stringify(this.state.data) : null}
-    </div>);
-  }
-}
+const base = '/Users/stephenshank/Documents/hyphy-vision/';
+
+const HVABSREL = () => (
+  <BSREL url={'methods/absrel/data/ABSREL.json'} hyphy_vision />
+);
+
+const HVBUSTED = () => (
+  <BUSTED url={'/methods/busted/data/BUSTED.json'} hyphy_vision />
+);
+
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+    <a href="/absrel">aBSREL</a>
+    <a href="/busted">BUSTED</a>
+  </div>
+);
+
+const HyphyVision = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Home}/>
+      <Route path="/absrel" component={HVABSREL}/>
+      <Route path="/busted" component={HVBUSTED}/>
+    </div>
+  </Router>
+);
 
 ReactDOM.render(
-  <Hello />,
+  <HyphyVision />,
   document.body.appendChild(document.createElement('div'))
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ config = {
     ]
   },
   devServer: {
+    historyApiFallback: true,
     contentBase: '.'
   },
   output: {
