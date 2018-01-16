c314065acd0185c6cf1df6c2c3304cd3f3a44066
Stephen Shank <sshank314@gmail.com>
2018-01-16 10:20:27 -0500

    Modify electron.js.

--- electron.js
+++ electron.js
@@ -18,7 +18,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: 'index.html',
     protocol: 'file:',
     slashes: true
   }))
@@ -38,7 +38,15 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  protocol.interceptFileProtocol('file', (request, callback) => {
+    const url = request.url.substr(7)    /* all urls start with 'file://' */
+    callback({ path: path.normalize(`${__dirname}/dist/${url}`)})
+  }, (err) => {
+    if (err) console.error('Failed to register protocol')
+  });
+  createWindow();
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
