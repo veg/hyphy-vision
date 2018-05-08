f1ba1cd56bd4ff8124f46233ae25b3c0de67bb3b
Stephen Shank <sshank314@gmail.com>
2018-05-08 17:02:55 -0400

    Restores JavaScript functionality to scrollspy, breaks CSS.

--- src/jsx/components/scrollspy.jsx
+++ src/jsx/components/scrollspy.jsx
@@ -8,34 +8,32 @@ react-router-hash-link doesn't allow for offsets to account for headers.
 React-router-hash-link is therefore used for Windows-Electron and 
 react-scrollchor is used for every other platform.
 */
-const configFile = require('../../../config.json');
-function scrollElement(href, item, configFile) {
-  if (configFile["env"] == "windows electron") {
-    return <Link to={href}>{item.label}</Link> 
-  }else{
-    return <Scrollchor animate={{duration: 20}} to={href}>{item.label}</Scrollchor>
-  }
-}; 
 
-var ScrollSpy = React.createClass({
-  render: function() {
-    var list_items = this.props.info.map(function(item, index) {
-      var is_active = index == 0 ? "active" : "",
-        href = "#" + item.href;
-      return (
-        <li className={is_active} key={item.label}>
-          {scrollElement(href, item, configFile)}          
-        </li>
-      );
-    });
+const configFile = require('../../../config.json');
+
+function ScrollSpy(props) {
+  var list_items = props.info.map(function(item, index) {
+    var is_active = index == 0 ? "active" : "",
+      href = "#" + item.href;
     return (
-      <nav className="col-sm-2 bs-docs-sidebar list-group d-xs-none d-sm-none d-md-none d-lg-none d-xl-block">
-        <ul className="nav nav-pills nav-stacked fixed flex-column">
-          {list_items}
-        </ul>
-      </nav>
+      <li className="nav-item" key={item.label}>
+        {configFile["env"] == "windows electron" ?
+          (<Link className={is_active + " nav-link"} to={href}>
+            {item.label}
+          </Link>) :
+          (<Scrollchor className={is_active + " nav-link"} animate={{duration: 20}} to={href}>
+            {item.label}
+          </Scrollchor>) }
+      </li>
     );
-  }
-});
+  });
+  return (
+    <nav className="col-sm-2 bs-docs-sidebar list-group d-xs-none d-sm-none d-md-none d-lg-none d-xl-block">
+      <ul className="nav nav-tabs nav-pills nav-stacked fixed flex-column">
+        {list_items}
+      </ul>
+    </nav>
+  );
+}
 
 module.exports.ScrollSpy = ScrollSpy;
