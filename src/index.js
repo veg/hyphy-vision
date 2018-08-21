0b0e808143348d72c1d3c764b6a4e24f5a648747
rdvelazquez <ryan.velazquez0001@temple.edu>
2018-08-21 12:07:38 -0400

    remove vision.hyphy.org specific scss from library build

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 require("font-awesome/css/font-awesome.css");
-require("./hyphyvision.css");
 require("./fade/FADE.css");
 require("bootstrap");
 require("./datamonkey/datamonkey.js");
 require("./application.scss");
+require("./website.scss");
 
 import render_app from "./jsx/app.jsx";
 
