4c92bd98a875ed2a1074b88fe33e7f5164de9880
stephenshank <sshank314@gmail.com>
2020-03-25 08:59:53 -0400

    Fix server.js (serve all requests to dist directory).

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.static("."));
-app.use(express.static("public"));
+app.use(express.static("dist"));
 
-app.get("/:methods/?", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 
 app.listen(port, () => {
